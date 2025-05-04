667e036c6d23cd90f9951637854aa1d28092af7b
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2025-05-04 17:40:22 -0300

    serviço mqtt

---
+++ first-spring-app/src/main/java/com/example/first_spring_app/service/WebSocketService.java
@@ -0,0 +1,24 @@
+package com.example.first_spring_app.service;
+
+import com.example.first_spring_app.model.SensorData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class WebSocketService {
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    public void sendSensorUpdate(SensorData sensorData) {
+        String topic = "/topic/sensor/" + sensorData.getDeviceId();
+        log.debug("Enviando atualização para WebSocket: {}", topic);
+        messagingTemplate.convertAndSend(topic, sensorData);
+        
+        // Enviar para um tópico geral para atualizações de todos os sensores
+        messagingTemplate.convertAndSend("/topic/sensors", sensorData);
+    }
+}
